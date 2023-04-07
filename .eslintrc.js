3ad8c33553c4d05a7087cdc830e1478de749da5a
Oselskyi Oleksandr <oselskyi3@gmail.com>
2023-04-07 14:27:18 +0300

    TodoApp

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,37 +1,12 @@
 module.exports = {
-  extends: [
-    '@mate-academy/eslint-config-react-typescript',
-    'plugin:react/recommended',
-  ],
+  extends: ['@mate-academy/eslint-config-react-typescript', 'plugin:cypress/recommended'],
   rules: {
-    // React
-    'react/prop-types': 0,
-    'react/self-closing-comp': 0,
-    'react/display-name': 0,
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-uses-react': 'off',
-    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx', '.ts', '.tsx'] }],
-
-    // JavaScript
-    semi: 0,
-    'no-proto': 0,
-    'no-unused-vars': 0,
-
-    // TypeScript
-    '@typescript-eslint/no-shadow': 'off',
-    '@typescript-eslint/no-unused-vars': ['error'],
-    '@typescript-eslint/explicit-module-boundary-types': 0,
-    '@typescript-eslint/ban-types': 0,
-    '@typescript-eslint/semi': ['error'],
-    'no-underscore-dangle': 'off',
-    'class-methods-use-this': 'off',
-    'import/no-extraneous-dependencies': [
-      'error',
-    ],
-    parser: '@typescript-eslint/parser',
-    plugins: [
-      '@typescript-eslint',
-    ],
+    'max-len': ['error', {
+      ignoreTemplateLiterals: true,
+      ignoreComments: true,
+    }],
+    'jsx-a11y/label-has-associated-control': ["error", {
+      assert: "either",
+    }],
   },
-}
-
+};
